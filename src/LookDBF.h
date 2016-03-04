835785b756155ed9ba022ca06f9f5df720dce8dd
Andrew Nefedkin <andrew.nefedkin@gmail.com>
2016-03-04 17:40:33 +0300

    Recompiled under VC++/VS2012.

---
+++ src/LookDBF.h
@@ -0,0 +1,315 @@
+#define WIN32_LEAN_AND_MEAN
+
+#include "db_use.h"
+#include "plugin.hpp"
+
+#define DTFmtL 32
+
+#define WinCode       0x00000001 // Win or Dos coding
+#define FindAllFields 0x00000002 // Search in all fields
+#define FindCaseSens  0x00000004 // CaseSensitive searching
+#define ExpHeads      0x00000008 // Field names export flag
+#define ExpActual     0x00000010 // Only actual records export flag
+#define ExpSpaces     0x00000020 // Spaces export flag
+#define ExpSeparator  0x00000040 // Use separator with text export flag
+#define ExpFileType   0x00000080 // Change File type with dbf export flag
+#define CondSearch    0x00000100 // Conditional search flag
+#define LineNums      0x00000200 // Show record numbers flag
+#define FindInvert    0x00000400 // Find inversion flag
+#define FullMemo      0x00000800 // Full screen memo showing flag
+#define WholeWords    0x00001000 // Whole words only searching
+#define FindReplace   0x00002000 // Search & replace mode
+#define ConfReplace   0x00004000 // Replace Confirmation
+#define AutoSave      0x00008000 // Tepmlate Autosave
+#define ExpEmpty      0x00010000 // Export empty file
+
+enum {
+	mColors,
+	//-------------------- Errors
+	mError, mNoOpen, mBadFile, mBadWrite, mNoMemory, mNoSelect, mNoNumeric,
+	mErExport, mLookOnly, mNoMemFile, mNoMemBlock, mBadRepl, mNoTemplate,
+	//-------------------- Buttons
+	mButOK, mButFind, mButSave, mButAdd, mButCancel,
+	//-------------------- Function Keys
+	mBarF1, mBarF2, mBarF4, mBarF6, mBarF7, mBarF9, mBarF10,
+	mBarSF7, mBarHF5, mBarAF5,
+	//----------------------- Configuration
+	mCfgCodeDef, mCfgRecNum, mCfgMemo, mCfgSep, mCfgAutoSave, mCfgShow, mCfgEdit,
+	mCfgCodeTable, mCfgMemExt,
+	//----------------------- Information
+	mFileInfo, mEmpty, mFileType, mLastUpdate, mIndexFile, mHeadLen, mRecLen,
+	mNumField, mFieldHead, mFieldEmpty,
+	//----------------------- Replace
+	mReplTitle, mReplFind, mReplRepl, mReplWords, mReplConf, mReplMask, mReplMark,
+	mReplUn, mReplAll, mReplAsk, mReplYes,
+	//----------------------- Search
+	mFindTitle, mFindAll, mFindCase, mFindFirst, mFindMark, mFindUnmark,
+	mFindInvert, mFindNo, mFindYes,
+	//----------------------- Packing
+	mPkTitle, mPkGood,
+	//----------------------- Export
+	mExTitle, mExName, mExFormat, mExCoding, mExOriginal, mExHead, mExSpace, mExSep,
+	mExActual, mExGood, mExBuff, mExpEmpty, mEsc,
+	//----------------------- Import
+	mImpTitle, mImpFileName,
+	//----------------------- Others
+	mYes, mNo, mAll, mColIns, mSum, mColName, mEditTitle, mTempFile, mGoTo, mTemplate,
+	//----------------------- Html-taggs
+	mTabS, mTabF, mTabRowS, mTabRowF, mTabCellS, mTabCellF, mTabSpace
+};
+//===========================================================================
+
+
+const char *GetMsg(int MsgId);
+//===========================================================================
+
+typedef char Str256[256];
+
+union CondValue {
+	__int64 i64;
+	double  dbl;
+	DWORD   dw[2];
+	BYTE    bt[256];
+};
+
+struct RecHead {
+	WORD Kind;        // Code of record kind
+	WORD Len;         // Record length
+};
+
+struct FindData {
+	short Pos;        // Position of found sample
+	short Step;       // Increment of Position of found sample
+	short Len;        // Searching Sample Length
+	short LenF;       // Found String Length
+	short nMM;        // Number of MultyMask characters in sample
+	short nMMr;       // Number of MultyMask characters in replase substring
+	char Mask[2];     // Masking Characters
+	char FD[256];     // Searching Text OEM coding
+	char FU[256];     // Searching Text coding ready for compare
+	char RU[256];     // Replace Text coding ready for replace
+	void Clear(void); // Clear structure
+};
+
+struct CondData {
+	WORD Oper;        // Operation bitween relations
+	WORD Field[2];    // Field numbers for conditions
+	WORD Rel[2];      // Relations
+	Str256 Str[2];    // Strings for compare
+	CondValue V[2];   // Values for compare
+};
+
+struct ExportData {
+	char File[MAX_PATH];  // Export file name
+	char Sep[2];          // Export file separator
+	char Type[2];         // Export file type for DBF export
+	char code;            // Export file coding operation
+	char mode;            // Export mode
+	WORD Form;            // Export file type (txt, htm or dbf)
+	WORD CoType;          // Export file coding type;
+	short fi[3];          // Field numbers for sorting (-1 - no sorting)
+	short dir[3];         // Sorting direction 0 - (0->99), 1 - (99->0)
+	DWORD recnum;         // Current record number
+	DWORD count;          // Count of exported records
+	short BufLim;         // Sorting buffer size
+	short BufLast;        // Records q-ty in Sorting buffer
+	short BufCurr;        // Record number for output from Sorting buffer
+	DWORD *RN;            // Record numbers buffer
+	CondValue *V0, *V1, *V2;// Sorting keys buffers
+};
+
+struct Indicator {
+	DWORD limit, count;
+	WORD tot, already, X;
+	void Start(const char *title, DWORD lim);
+	bool Move(DWORD step);
+};
+
+struct Column :Link {
+	short wid;
+	short finum;
+	short pos;
+	BYTE dinum, idnum;
+	char  name[26];
+	void Put(Column *c);
+};
+
+struct LOOK {
+	Column *C;        // Array of columns information
+	CHAR_INFO *VBuf;  // Screen virtual buffer
+	DWORD *recV;      // Numbers of visible records
+	WORD   *M;        // Array of record marks
+	WORD   *S;        // Array of record sorted marks
+	dbBase db;        // Data Base structure
+
+	short LookOnly;   // Prohibit edit, append and detete functions
+	short MarkOnly;   // On/Off marked records only mode of displaying
+	DWORD MarkNum;    // Number of marked records
+	DWORD MarkMax;    // Maximum number of marked records
+	short sw, sh;      // Screen width and height-1
+	BYTE  at[24];     // Colors array
+	short Wrec;       // Width of current line number showing
+	short Wcol;       // Width of current column number showing
+	short FindMax;    // Maximum Length of Searching Text showing
+	short sXcode;     // X of Win/Dos coding showing
+	short sXfind;     // X of find string showing
+	short sXcol;      // X of columns number showing
+	short sXrec;      // X of current record number showing
+
+	WORD HidNum;      // Number of hidden columns
+	Column *Hid;      // Array of hidden columns
+	Column *coFirst;  // Pointer to 1 visible column structure
+	Column *coCurr;   // Pointer to current column structure
+	Column *coLast;   // Pointer to Last visible column
+	short coTail;     // Number of reduced characters in Last visible column
+	short Xcur;       // Current column start coordinate
+	short curX;       // Current column number
+	short curY;       // Current line number
+	short botY;       // Last line number
+	char FileName[MAX_PATH];
+
+	DWORD Flags;          // Any Flags Set
+	DWORD FlagsEntry;     // Flags Set without template influence
+	FindData Find;        // Find data container
+	CondData Cond;        // Condition data container
+	ExportData Exp;       // Export data container
+	int ctsNum;           // Decoding table number
+	CharTableSet *ChaTa;  // Pointer to decoding table
+	WORD(LOOK::*ExpRec)(void);
+	Indicator Indic;
+
+	void Set(DWORD flg) { Flags |= flg; }
+	void Clear(DWORD flg) { Flags &= ~flg; }
+	DWORD Yes(DWORD flg) { return Flags&flg; }
+	DWORD No(DWORD flg) { return !(Flags&flg); }
+	bool YesAlphaNum(BYTE c);
+
+	WORD SortAlloc(void);
+	void SortFree(void);
+	void SortSet(DWORD recnum);
+	void SortClear(DWORD recnum);
+	bool Sorted(DWORD recnum);
+	void SortSetAll(void);
+	void SortClearAll(void);
+	void SortSetMarked(void);
+
+	WORD MarkAlloc(void);
+	void MarkFree(void);
+	void MarkSet(DWORD recnum);
+	void MarkClear(DWORD recnum);
+	void MarkInvert(DWORD recnum);
+	bool Marked(DWORD recnum);
+	void MarkSetAll(void);
+	void MarkClearAll(void);
+	void MarkInvertAll(void);
+	void MarkFindFirst(void);
+	void MarkNonActual(void);
+
+	WORD GoNextMark(void);
+	WORD GoPrevMark(void);
+	WORD GoUp(WORD delta);
+	WORD GoDn(WORD delta);
+	WORD GoTop(void);
+	WORD GoBot(void);
+	WORD GoLeft(void);
+	WORD GoRight(void);
+	WORD GoFirst(void);
+	WORD GoLast(void);
+	WORD GoFind(short ma = 3);
+	WORD GoField(int id);
+	WORD GoAsk(void);
+	WORD GoMouse(MOUSE_EVENT_RECORD *m);
+	WORD TopMouse(int Line, MOUSE_EVENT_RECORD *m);
+
+	bool BuildBuffers(bool ResizeFlag = false);
+	void InitColumns(void);
+	void GetScreenSize(void);
+	void GetConfig(bool inrun = false);
+	void DefColors(char *u);
+	void AttrRect(WORD left, WORD top, WORD width, WORD height, WORD Attr);
+	void AttrLine(WORD left, WORD width, WORD Y, WORD Attr);
+	void ClearRect(WORD left, WORD top, WORD width, WORD height);
+
+	short ReplaceDlg(void);
+	short ReplaceAsk(void);
+	short Replace(void);
+	short ReplaceStr(char *cc);
+	void ReplaceAll(short ma, DWORD mrn);
+	bool NotRepl(DWORD rn, short ma);
+	short FindAskSample(void);
+	DWORD FindNext(short *f, short ma);
+	DWORD FindMark(WORD m);
+	short FindCompare(WORD fn);
+	short CondAsk(void);
+	void CondVal(WORD n);
+	bool Like(BYTE *sample, BYTE *line);
+	bool CondCheck(short n, BYTE *fi);
+	bool CondCheck(short n, __int64 fi);
+	bool CondCheck(short n, double fi);
+	bool CondCheck(short n, DWORD fi1, DWORD fi2);
+	bool CondCompare(short n);
+	bool CondYes(void);
+	DWORD CondNext(void);
+
+	Column *FindFin(short fin);
+	Column *FieldItem(short it);
+	char *FieldItemType(short it, char *buf);
+
+	WORD Export(void);
+	WORD ExpTxt(void);
+	WORD ExpHtm(void);
+	WORD ExpDbf(void);
+	WORD PackDbf(void);
+	char *NameScratch(char *nm);
+	WORD ExpRec0(void);
+	WORD ExpRec1(void);
+	WORD ExpRec2(void);
+	void FieldValue(WORD finum, CondValue *val);
+	short FieldCompare(short finum, CondValue *v1, CondValue *v2);
+	WORD Import(void);
+
+	WORD ClmnDelete(void);
+	WORD ClmnInsert(void);
+	WORD ClmnMove(char d);
+	WORD ClmnNarrow(void);
+	WORD ClmnEnlarge(void);
+	WORD ClmnRename(void);
+	WORD ClmnScroll(short d);
+
+	WORD ActualRecord(void);
+	WORD ActualSelected(BYTE a);
+	WORD EditRecord(void);
+	WORD EditField(BYTE nll = 0);
+	void EditHeader(void);
+	char CurType(void);
+	void KeyShow(void);
+	void ChangeCode(void);
+	void Clipboard(void);
+	void ClearCur(void);
+
+	int AskMsg(char *s);
+	int OkMsg(char *s);
+	void ShowCur(void);
+	void ShowError(int index);
+	void ShowExpMsg(DWORD msec);
+	void ShowFindMsg(DWORD nr);
+	void ShowReplMsg(DWORD nr);
+	void ShowPage(void);
+	int  ShowFields(void);
+	void ShowMemo(short id);
+	void ShowStr(const char *str, WORD x, WORD y, BYTE atn = 0, WORD L = 0);
+	void ShowStrI(const char *str, WORD x, WORD y, WORD L);
+	void ShowChar(char c, WORD x, WORD y, BYTE atn = 0);
+	void ShowF5(void);
+	void ShowStatus(int index);
+	void ShowSum(void);
+	void ShowDBF(void);
+	void NameTemplate(char *nm);
+	void GetTemplate(void);
+	void PutTemplate(bool msg = false);
+	void DelTemplate(void);
+	int  TableSelect(void);
+	int  TableSet(int nt);
+	void ToOem(TCHAR *src, TCHAR *dst = NULL);
+	void ToAlt(TCHAR *src, TCHAR *dst = NULL);
+};
---
+++ src/Plugin.cpp
@@ -0,0 +1,357 @@
+#include "LookDBF.h"
+
+PluginStartupInfo Info;
+FarStandardFunctions FSF;
+HANDLE LookHeap;
+const char *Title = "LookDBF";
+char *RootKey;
+
+LOOK *data;
+
+extern const char *DefD;
+extern const char *DefT;
+extern const char *DefMemExt;
+//===========================================================================
+
+/*
+Функция GetMsg возвращает строку сообщения из языкового файла.
+А это надстройка над Info.GetMsg для сокращения кода :-)
+*/
+const char *GetMsg(int MsgId)
+{
+	return Info.GetMsg(Info.ModuleNumber, MsgId);
+}
+//===========================================================================
+
+int WINAPI _export GetMinFarVersion(void)
+{
+	return MAKEFARVERSION(1, 75, 2634); //FARMANAGERVERSION
+}
+
+/*
+Функция SetStartupInfo вызывается один раз, перед всеми
+другими функциями. Она передает плагину информацию,
+необходимую для дальнейшей работы.
+*/
+void WINAPI _export SetStartupInfo(const struct PluginStartupInfo *Info)
+{
+	::Info = *Info;
+	::FSF = *Info->FSF;
+	::Info.FSF = &::FSF;
+
+	RootKey = (TCHAR*) HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, (lstrlen(Info->RootKey) + 8) * sizeof(TCHAR) + 1);
+	lstrcpy(RootKey, Info->RootKey);
+	FSF.AddEndSlash(RootKey);
+	lstrcat(RootKey, TEXT("LookDBF"));
+}
+//===========================================================================
+
+/*
+Функция GetPluginInfo вызывается для получения основной
+(general) информации о плагине
+*/
+void WINAPI _export GetPluginInfo(struct PluginInfo *pi)
+{
+	static const char *PluginMenuStrings[1];
+	pi->StructSize = sizeof(struct PluginInfo);
+	PluginMenuStrings[0] = Title;
+	pi->PluginMenuStrings = PluginMenuStrings;
+	pi->PluginMenuStringsNumber = 1;
+	pi->PluginConfigStrings = PluginMenuStrings;
+	pi->PluginConfigStringsNumber = 1;
+	pi->CommandPrefix = PluginMenuStrings[0];
+}
+//===========================================================================
+
+int WINAPI _export Configure(int ItemNumber)
+{
+	int i, n;
+	LONG rz;
+	HKEY hKey;
+	DWORD val, sz;
+	CharTableSet cts;
+	FarDialogItem di[34];
+	FarList flc, flt;
+	FarListItem fli[2];
+	ZeroMemory(di, sizeof(di));
+	ZeroMemory(fli, sizeof(fli));
+	flc.Items = fli; flc.ItemsNumber = 2;
+	lstrcpy(fli[0].Text, "Win"); lstrcpy(fli[1].Text, "Dos");
+	if (ItemNumber >= 0) {
+		LookHeap = HeapCreate(HEAP_NO_SERIALIZE, 0x20000, 0);
+		if (!LookHeap)return false;
+	}
+	n = 1;
+	while (Info.CharTable(n - 1, (char*) (&cts), sizeof(CharTableSet)) >= 0)n++;
+	flt.Items = new FarListItem[n];
+	flt.ItemsNumber = n;
+	lstrcpy(flt.Items[0].Text, "Current Windows Code Table");
+	for (i = 1; i < n; i++) {
+		Info.CharTable(i - 1, (char*) (&cts), sizeof(CharTableSet));
+		lstrcpy(flt.Items[i].Text, cts.TableName);
+	}
+	fli[0].Flags = LIF_SELECTED; //  Кодировка по умолчанию (Win)
+	di[3].Selected = 0;          //  Показывать номера записей (нет)
+	di[4].Selected = 0;          //  Показывать мемо на весь экран (нет)
+	di[5].Data[0] = 0xb3;        //  Разделитель эксопрта
+	di[7].Selected = 0;          //  Автоматически сохранять шаблон (нет)
+	lstrcpy(di[8].Data, "?");   // маскирующий символ
+	lstrcpy(di[14].Data, DefD); // Показ даты
+	lstrcpy(di[15].Data, DefT); // Показ даты-времени
+	lstrcpy(di[18].Data, DefD); // Ввод даты
+	lstrcpy(di[19].Data, DefT); // Ввод даты-времени
+	lstrcpy(di[22].Data, DefD); // Экспорт даты
+	lstrcpy(di[23].Data, DefT); // Экспорт даты-времени
+	di[26].Data[0] = 0;          // Цвета пользователя
+	flt.Items[0].Flags = LIF_SELECTED; // Таблица по умолчанию - текущая Windows
+	RegCreateKeyEx(HKEY_CURRENT_USER, RootKey, 0,
+				   NULL, REG_OPTION_NON_VOLATILE, KEY_ALL_ACCESS, NULL, &hKey, &sz);
+	if (sz != REG_CREATED_NEW_KEY) {
+		sz = sizeof(val);   //---------  Кодировка по умолчанию (Win)
+		rz = RegQueryValueEx(hKey, "CodeDefault", NULL, NULL, (BYTE*) (&val), &sz);
+		if (rz == ERROR_SUCCESS) {
+			if (val)fli[0].Flags = LIF_SELECTED; else fli[1].Flags = LIF_SELECTED;
+		}
+		sz = sizeof(val);  //---------  Показывать номера записей (нет)
+		rz = RegQueryValueEx(hKey, "ShowRecNum", NULL, NULL, (BYTE*) (&val), &sz);
+		if (rz == ERROR_SUCCESS) di[3].Selected = val;
+		sz = sizeof(val);  //----------  Показывать мемо на весь экран (нет)
+		rz = RegQueryValueEx(hKey, "ShowMemoFull", NULL, NULL, (BYTE*) (&val), &sz);
+		if (rz == ERROR_SUCCESS)di[4].Selected = val;
+		sz = 3;            //----------- Разделитель экспорта
+		rz = RegQueryValueEx(hKey, "ExpSep", NULL, NULL, (BYTE*) di[5].Data, &sz);
+		if (rz != ERROR_SUCCESS) { di[5].Data[0] = 0xb3; di[5].Data[1] = 0; }
+		sz = sizeof(val);  //-----------  Автоматически сохранять шаблон (нет)
+		rz = RegQueryValueEx(hKey, "AutoSave", NULL, NULL, (BYTE*) (&val), &sz);
+		if (rz == ERROR_SUCCESS)di[7].Selected = val;
+		sz = 3;            //----------- Маскирующие символы
+		rz = RegQueryValueEx(hKey, "MaskChar", NULL, NULL, (BYTE*) di[6].Data, &sz);
+		if (rz != ERROR_SUCCESS) { di[8].Data[0] = '?'; di[8].Data[1] = '%'; }
+		if (!di[8].Data[1])di[8].Data[1] = '%'; di[8].Data[2] = 0;
+		sz = 256;       //----------- Пользовательские цвета
+		rz = RegQueryValueEx(hKey, "Colors", NULL, NULL, (BYTE*) di[26].Data, &sz);
+		if (rz != ERROR_SUCCESS) di[26].Data[0] = 0;
+		else di[26].Data[sz] = 0;
+		sz = 256;       //----------- Расширения мемо-файла
+		rz = RegQueryValueEx(hKey, "MemExt", NULL, NULL, (BYTE*) di[30].Data, &sz);
+		if (rz != ERROR_SUCCESS) lstrcpy(di[30].Data, DefMemExt);
+		else di[30].Data[sz] = 0;
+		sz = DTFmtL;       //----------- Показ даты
+		rz = RegQueryValueEx(hKey, "D_fmt_V", NULL, NULL, (BYTE*) di[14].Data, &sz);
+		if (rz != ERROR_SUCCESS) lstrcpy(di[14].Data, DefD);
+		else di[14].Data[sz] = 0;
+		sz = DTFmtL;           //----------- Показ даты-времени
+		rz = RegQueryValueEx(hKey, "T_fmt_V", NULL, NULL, (BYTE*) di[15].Data, &sz);
+		if (rz != ERROR_SUCCESS) lstrcpy(di[15].Data, DefT);
+		else di[15].Data[sz] = 0;
+		sz = DTFmtL;       //----------- Ввод даты
+		rz = RegQueryValueEx(hKey, "D_fmt", NULL, NULL, (BYTE*) di[18].Data, &sz);
+		if (rz != ERROR_SUCCESS) lstrcpy(di[18].Data, DefD);
+		else di[18].Data[sz] = 0;
+		sz = DTFmtL;           //----------- Ввод даты-времени
+		rz = RegQueryValueEx(hKey, "T_fmt", NULL, NULL, (BYTE*) di[19].Data, &sz);
+		if (rz != ERROR_SUCCESS) lstrcpy(di[19].Data, DefT);
+		else di[19].Data[sz] = 0;
+		sz = DTFmtL;       //----------- Экспорт даты
+		rz = RegQueryValueEx(hKey, "D_fmt_E", NULL, NULL, (BYTE*) di[22].Data, &sz);
+		if (rz != ERROR_SUCCESS) lstrcpy(di[22].Data, DefD);
+		else di[22].Data[sz] = 0;
+		sz = DTFmtL;           //----------- Экспорт даты-времени
+		rz = RegQueryValueEx(hKey, "T_fmt_E", NULL, NULL, (BYTE*) di[23].Data, &sz);
+		if (rz != ERROR_SUCCESS) lstrcpy(di[23].Data, DefT);
+		else di[23].Data[sz] = 0;
+		sz = sizeof(val);   //---------  Таблица по умолчанию  Current Win
+		rz = RegQueryValueEx(hKey, "CodeTable", NULL, NULL, (BYTE*) (&val), &sz);
+		if (rz == ERROR_SUCCESS && val < n) {
+			flt.Items[0].Flags = 0;
+			flt.Items[val].Flags = LIF_SELECTED;
+			if (val)lstrcpy(fli[0].Text, "Alt");
+		}
+	}
+	i = 0; //0
+	di[i].Type = DI_DOUBLEBOX; di[i].X1 = 3; di[i].X2 = 50;
+	di[i].Y1 = 1; di[i].Y2 = 22;
+	lstrcpy(di[i].Data, Title);
+	++i; //1
+	di[i].Type = DI_COMBOBOX; di[i].X1 = 5; di[i].Y1 = 2; di[i].X2 = 7;
+	di[i].Flags = DIF_DROPDOWNLIST; di[i].ListItems = &flc;
+	++i; //2
+	di[i].Type = DI_TEXT; di[i].X1 = 9; di[i].Y1 = 2;
+	lstrcpy(di[i].Data, GetMsg(mCfgCodeDef));
+	++i; //3
+	di[i].Type = DI_CHECKBOX; di[i].X1 = 5; di[i].Y1 = 3;
+	lstrcpy(di[i].Data, GetMsg(mCfgRecNum));
+	++i; //4
+	di[i].Type = DI_CHECKBOX; di[i].X1 = 5; di[i].Y1 = 4;
+	lstrcpy(di[i].Data, GetMsg(mCfgMemo));
+	++i; //5
+	di[i].Type = DI_FIXEDIT; di[i].X1 = 5; di[i].X2 = 7; di[i].Y1 = 5;
+	++i; //6
+	di[i].Type = DI_TEXT; di[i].X1 = 9; di[i].Y1 = 5;
+	lstrcpy(di[i].Data, GetMsg(mCfgSep));
+	++i; //7
+	di[i].Type = DI_CHECKBOX; di[i].X1 = 5; di[i].Y1 = 6;
+	lstrcpy(di[i].Data, GetMsg(mCfgAutoSave));
+	++i; //8
+	di[i].Type = DI_FIXEDIT; di[i].X1 = 5; di[i].X2 = 7; di[i].Y1 = 7;
+	++i; //9
+	di[i].Type = DI_TEXT; di[i].X1 = 9; di[i].Y1 = 7;
+	lstrcpy(di[i].Data, GetMsg(mReplMask));
+	++i; //10
+	di[i].Type = DI_TEXT; di[i].X1 = 5; di[i].Y1 = 9;
+	lstrcpy(di[i].Data, GetMsg(mExFormat)); lstrcat(di[i].Data, ":");
+	++i; //11
+	di[i].Type = DI_TEXT; di[i].X1 = 14; di[i].Y1 = 9;
+	lstrcpy(di[i].Data, "D (Date)        T (DateTime)");
+
+	++i; //12
+	di[i].Type = DI_TEXT; di[i].Y1 = 10; di[i].Flags = DIF_SEPARATOR;
+	++i; //13
+	di[i].Type = DI_TEXT; di[i].X1 = 5; di[i].Y1 = 11;
+	lstrcpy(di[i].Data, GetMsg(mCfgShow));
+	++i; //14
+	di[i].Type = DI_EDIT; di[i].X1 = 13; di[i].X2 = di[i].X1 + 10; di[i].Y1 = 11;
+	++i; //15
+	di[i].Type = DI_EDIT; di[i].X1 = 25; di[i].X2 = di[i].X1 + 23; di[i].Y1 = 11;
+
+	++i; //16
+	di[i].Type = DI_TEXT; di[i].Y1 = 12; di[i].Flags = DIF_SEPARATOR;
+	++i; //17
+	di[i].Type = DI_TEXT; di[i].X1 = 5; di[i].Y1 = 13;
+	lstrcpy(di[i].Data, GetMsg(mCfgEdit));
+	++i; //18
+	di[i].Type = DI_EDIT; di[i].X1 = 13; di[i].X2 = di[i].X1 + 10; di[i].Y1 = 13;
+	++i; //19
+	di[i].Type = DI_EDIT; di[i].X1 = 25; di[i].X2 = di[i].X1 + 23; di[i].Y1 = 13;
+
+	++i; //20
+	di[i].Type = DI_TEXT; di[i].Y1 = 14; di[i].Flags = DIF_SEPARATOR;
+	++i; //21
+	di[i].Type = DI_TEXT; di[i].X1 = 5; di[i].Y1 = 15;
+	lstrcpy(di[i].Data, GetMsg(mExTitle));
+	++i; //22
+	di[i].Type = DI_EDIT; di[i].X1 = 13; di[i].X2 = di[i].X1 + 10; di[i].Y1 = 15;
+	++i; //23
+	di[i].Type = DI_EDIT; di[i].X1 = 25; di[i].X2 = di[i].X1 + 23; di[i].Y1 = 15;
+	++i; //24
+	di[i].Type = DI_TEXT; di[i].Y1 = 16; di[i].Flags = DIF_SEPARATOR;
+
+	++i; //25
+	di[i].Type = DI_TEXT; di[i].X1 = 5; di[i].Y1 = 17;
+	lstrcpy(di[i].Data, GetMsg(mColors));
+	++i; //26
+	di[i].Type = DI_EDIT; di[i].X1 = 13; di[i].X2 = di[i].X1 + 35; di[i].Y1 = 17;
+	++i; //27
+	di[i].Type = DI_TEXT; di[i].X1 = 5; di[i].Y1 = 18;
+	lstrcpy(di[i].Data, GetMsg(mCfgCodeTable));
+	++i; //28
+	di[i].Type = DI_COMBOBOX; di[i].X1 = 13; di[i].Y1 = 18; di[i].X2 = di[i].X1 + 35;
+	di[i].Flags = DIF_DROPDOWNLIST; di[i].ListItems = &flt;
+	++i; //29
+	di[i].Type = DI_TEXT; di[i].X1 = 5; di[i].Y1 = 19;
+	lstrcpy(di[i].Data, GetMsg(mCfgMemExt));
+	++i; //30
+	di[i].Type = DI_EDIT; di[i].X1 = 13; di[i].X2 = di[i].X1 + 35; di[i].Y1 = 19;
+	++i; //31
+	di[i].Type = DI_TEXT; di[i].Y1 = 20; di[i].Flags = DIF_SEPARATOR;
+
+	++i; //32
+	di[i].Type = DI_BUTTON; lstrcpy(di[i].Data, GetMsg(mButSave));
+	di[i].X1 = 0; di[i].Y1 = 21; di[i].Flags = DIF_CENTERGROUP;
+	di[i].DefaultButton = 1;
+	++i; //33
+	di[i].Type = DI_BUTTON; lstrcpy(di[i].Data, GetMsg(mButCancel));
+	di[i].X1 = 0; di[i].Y1 = 21; di[i].Flags = DIF_CENTERGROUP;
+	++i; //34
+	i = Info.Dialog(Info.ModuleNumber, -1, -1, 54, 24, "Config", di, i);
+	if (i == 32) {
+		val = 1 - di[1].ListPos; sz = 4;
+		RegSetValueEx(hKey, "CodeDefault", 0, REG_DWORD, (BYTE*) (&val), sz);
+		val = di[3].Selected; sz = 4;    //-----> Показывать номера записей
+		RegSetValueEx(hKey, "ShowRecNum", 0, REG_DWORD, (BYTE*) (&val), sz);
+		val = di[4].Selected; sz = 4;    //-----> Показывать мемо на весь экран
+		RegSetValueEx(hKey, "ShowMemoFull", 0, REG_DWORD, (BYTE*) (&val), sz);
+		sz = lstrlen(di[5].Data) + 1;    //-----> Разделитель экспорта
+		if (sz < 2 || ((di[5].Data[0] == 'B' || di[5].Data[0] == 'b') && di[5].Data[1] == '3')) {
+			di[5].Data[0] = 0xb3; di[5].Data[1] = 0; sz = 2;
+		}
+		if (sz > 3) { di[5].Data[2] = 0; sz = 3; }
+		if (sz)RegSetValueEx(hKey, "ExpSep", 0, REG_SZ, (BYTE*) di[5].Data, sz);
+		val = di[7].Selected; sz = 4;    //-----> Автоматически сохранять шаблон
+		RegSetValueEx(hKey, "AutoSave", 0, REG_DWORD, (BYTE*) (&val), sz);
+		di[8].Data[2] = 0;             //-----> Маскирующие символы
+		if (!di[8].Data[0]) { di[8].Data[0] = '?'; di[8].Data[1] = '%'; }
+		if (!di[8].Data[1]) di[8].Data[1] = '%';
+		sz = lstrlen(di[8].Data) + 1;
+		RegSetValueEx(hKey, "MaskChar", 0, REG_SZ, (BYTE*) di[8].Data, sz);
+		sz = lstrlen(di[26].Data) + 1;   //-----------> Пользовательские цвета
+		if (sz > 256)sz = 256;
+		RegSetValueEx(hKey, "Colors", 0, REG_SZ, (BYTE*) di[26].Data, sz);
+		val = di[28].ListPos; sz = 4;    //----------> Кодовая таблица
+		RegSetValueEx(hKey, "CodeTable", 0, REG_DWORD, (BYTE*) (&val), sz);
+		sz = lstrlen(di[30].Data) + 1;   //----------> Расширения мемо-файла
+		if (sz > 256) { sz = 256; di[30].Data[sz] = 0; }
+		RegSetValueEx(hKey, "MemExt", 0, REG_SZ, (BYTE*) di[30].Data, sz);
+		di[14].Data[DTFmtL - 1] = 0;            //-----> Показ даты
+		sz = lstrlen(di[14].Data) + 1;
+		RegSetValueEx(hKey, "D_fmt_V", 0, REG_SZ, (BYTE*) di[14].Data, sz);
+		di[15].Data[DTFmtL - 1] = 0;            //-----> Показ даты-времени
+		sz = lstrlen(di[15].Data) + 1;
+		RegSetValueEx(hKey, "T_fmt_V", 0, REG_SZ, (BYTE*) di[15].Data, sz);
+		di[18].Data[DTFmtL - 1] = 0;            //-----> Ввод даты
+		sz = lstrlen(di[18].Data) + 1;
+		RegSetValueEx(hKey, "D_fmt", 0, REG_SZ, (BYTE*) di[18].Data, sz);
+		di[19].Data[DTFmtL - 1] = 0;            //-----> Ввод даты-времени
+		sz = lstrlen(di[19].Data) + 1;
+		RegSetValueEx(hKey, "T_fmt", 0, REG_SZ, (BYTE*) di[19].Data, sz);
+		di[22].Data[DTFmtL - 1] = 0;            //-----> Экспорт даты
+		sz = lstrlen(di[22].Data) + 1;
+		RegSetValueEx(hKey, "D_fmt_E", 0, REG_SZ, (BYTE*) di[22].Data, sz);
+		di[23].Data[DTFmtL - 1] = 0;            //-----> Экспорт даты-времени
+		sz = lstrlen(di[23].Data) + 1;
+		RegSetValueEx(hKey, "T_fmt_E", 0, REG_SZ, (BYTE*) di[23].Data, sz);
+	}
+	RegCloseKey(hKey);
+	delete flt.Items;
+	if (ItemNumber >= 0) { HeapDestroy(LookHeap); LookHeap = 0; }
+	return false;
+}
+
+/*
+Функция OpenPlugin вызывается при создании новой копии плагина.
+*/
+HANDLE WINAPI _export OpenPlugin(int OpenFrom, INT_PTR Item)
+{
+	LookHeap = HeapCreate(HEAP_NO_SERIALIZE, 0x20000, 0);
+	if (!LookHeap)return INVALID_HANDLE_VALUE;
+	char *a, b[MAX_PATH];
+	struct PanelInfo pi;
+	data = new LOOK;
+	if (!data)goto PLUG_EXIT;
+	if (OpenFrom == OPEN_COMMANDLINE) {
+		lstrcpy(b, (const char *) Item); FSF.Unquote(b);
+		data->Set(WinCode); data->LookOnly = 1;
+		for (a = b;; a++) {
+			if (*a == ' ') continue;
+			if (*a != '/') break;
+			switch (*(++a)) {
+			case 'd': case 'D':  data->Clear(WinCode); break;
+			case 'e': case 'E':  data->LookOnly = 0; break;
+			case 'n': case 'N':  data->Set(LineNums); break;
+			case 'm': case 'M':  data->Set(FullMemo); break;
+			}
+		}
+		lstrcpy(data->FileName, a);
+		data->ShowDBF();
+	}
+	else if (OpenFrom == OPEN_PLUGINSMENU) {
+		Info.Control(INVALID_HANDLE_VALUE, FCTL_GETPANELINFO, (void *) &pi);
+		if (pi.Plugin || !pi.ItemsNumber) goto PLUG_EXIT;
+		lstrcpy(data->FileName, pi.PanelItems[pi.CurrentItem].FindData.cFileName);
+		data->ShowDBF();
+	}
+PLUG_EXIT:
+	if (LookHeap) HeapDestroy(LookHeap);
+	return  INVALID_HANDLE_VALUE;
+}
+//===========================================================================
