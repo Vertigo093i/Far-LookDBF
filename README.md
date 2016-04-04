7ea1b3cf93f090a4c1ce90f0089dcc04aad9bc93
Andrew Nefedkin <andrew.nefedkin@gmail.com>
2016-04-04 15:06:23 +0300

    Added PluginGUID to plugin's VersionInfo (for Far 3.0.3410).

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
-﻿Far-LookDBF
-===========
+﻿# LookDBF [![Build status](https://ci.appveyor.com/api/projects/status/8heypktos1ts9vnk?svg=true)](https://ci.appveyor.com/project/Vertigo093i/far-lookdbf)
 
 LookDBF is plug-in for Far Manager 1.75 and higher
-
-[![Build status](https://ci.appveyor.com/api/projects/status/8heypktos1ts9vnk?svg=true)](https://ci.appveyor.com/project/Vertigo093i/far-lookdbf)
--- src/version.h
+++ src/version.h
@@ -2,8 +2,11 @@
 
 #define PLUGIN_MAJOR_VER 2
 #define PLUGIN_MINOR_VER 7
-#define PLUGIN_BUILD 0
+#define PLUGIN_BUILD 1
 #define PLUGIN_NAME "LookDBF"
 #define PLUGIN_FILENAME PLUGIN_NAME ".dll"
 #define PLUGIN_DESC PLUGIN_NAME " Plugin for Far Manager"
 #define PLUGIN_COPYRIGHT "© 2003 Alexei Boltunov, © 2016 Andrew Nefedkin"
+#if FARMANAGERVERSION_MAJOR == 1
+#define PLUGIN_GUID "D50A91C5-5A97-4749-ADED-499C27EC649E"
+#endif
